49260c0ad7750ca257f6ce54a1d151247d79db02
kcenon <kcenon@naver.com>
2026-02-03 22:22:22 +0900

    refactor(internal): update WebSocket adapter includes to internal paths (#650)
    Update ws_connection_adapter.h and ws_listener_adapter.h to use
    internal header paths instead of deprecated public paths.
    
    Closes #649

--- src/internal/unified/adapters/ws_listener_adapter.h
+++ src/internal/unified/adapters/ws_listener_adapter.h
@@ -33,7 +33,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 
 #include "kcenon/network/unified/i_listener.h"
-#include "kcenon/network/http/websocket_server.h"
+#include "internal/http/websocket_server.h"
 
 #include <atomic>
 #include <memory>
